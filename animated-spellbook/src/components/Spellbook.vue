9c1d1f680977556541cdc013c4c485e5d4d89a60
Zachary Wilson <zlwilson21@gmail.com>
2020-12-07 13:04:12 -0500

    Adding selections

---
+++ animated-spellbook/src/components/Spellbook.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <form>
+            <label for="class">Please choose your class</label>
+            <select name="class" id="class">
+                <option value="Bard">Bard</option>
+                <option value="Cleric">Cleric</option>
+                <option value="Druid">Druid</option>
+                <option value="Rogue">Rogue</option>
+                <option value="Sorcerer">Sorcerer</option>
+                <option value="Warlock">Warlock</option>
+                <option value="Wizard">Wizard</option>
+                
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
